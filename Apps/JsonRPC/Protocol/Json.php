1f838de4da19ea0ed9d70838689e82f2a9e57e75
hidehalo <tianchen_cc@yeah.net>
2016-10-21 12:04:07 +0800

    restructed

--- Apps/JsonRPC/Protocol/Json.php
+++ Apps/JsonRPC/Protocol/Json.php
@@ -1,24 +1,7 @@
 <?php 
-/**
- * This file is part of workerman.
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the MIT-LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @author walkor<walkor@workerman.net>
- * @copyright walkor<walkor@workerman.net>
- * @link http://www.workerman.net/
- * @license http://www.opensource.org/licenses/mit-license.php MIT License
- */
 namespace Protocols;
 
-/**
- * RPC 协议解析 相关
- * 协议格式为 [json字符串\n]
- * @author walkor <worker-man@qq.com>
- * */
-class JsonNL
+class JsonRPC
 {
     /**
      * 检查包的完整性
--- start.php
+++ start.php
@@ -3,6 +3,13 @@
  * run with command 
  * php start.php start
  */
+define('APP',__DIR__'/Apps');
+$registy = array(
+    'jsonrpc'=>APP.'/JsonRPC/',
+    'statistics'=>APP.'/Statistics/'
+);
+if($argc == 0)
+    exit("请至少输入一个启动的服务");
 
 ini_set('display_errors', 'on');
 use Workerman\Worker;
@@ -10,12 +17,12 @@ use Workerman\Worker;
 // 检查扩展
 if(!extension_loaded('pcntl'))
 {
-    exit("Please install pcntl extension. See http://doc3.workerman.net/install/install.html\n");
+    exit("缺少PCNTL扩展");
 }
 
 if(!extension_loaded('posix'))
 {
-    exit("Please install posix extension. See http://doc3.workerman.net/install/install.html\n");
+    exit("缺少POSIX扩展");
 }
 
 // 标记是全局启动
@@ -24,9 +31,15 @@ define('GLOBAL_START', 1);
 require_once __DIR__ . '/Workerman/Autoloader.php';
 
 // 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
+foreach($argv as $index => $start_file)
 {
-    require_once $start_file;
+    if($index == 0)
+        continue;
+    if(in_array($start_file,$registy)){
+        require_once $registy[$start_file];
+    }else{
+        echo "服务 $start_file 不存在".PHP_EOL;
+    }
 }
 // 运行所有服务
 Worker::runAll();
